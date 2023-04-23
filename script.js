7609589b961dfa462cbbb95b32a9db146dbe1c14
himanshu1601 <hs3170118@gmail.com>
2023-04-23 20:19:44 +0530

    second commit

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Python Developer", "Data Scientist", "Data Analyst", "ML engineer", " AI Developer"],
+        strings: ["Python Developer", "Data Scientist", "Data Analyst", "ML Engineer", " AI Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Python Developer", "Data Scientist", "Data Analyst", "ML engineer", " AI Developer"],
+        strings: ["Python Developer", "Data Scientist", "Data Analyst", "ML Engineer", " AI Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
